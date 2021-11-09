88296716e8d9ccf3ce89631d219e11309dba8195
Graeme Brown <graemebrown1874@gmail.com>
2021-11-09 19:54:45 +0000

    first commit - boiler plate partly complete

---
+++ models/album.py
@@ -0,0 +1,7 @@
+class Album():
+    
+    def __init__(self, title, genre, artist_id, id = None):
+        self.title = title
+        self.genre = genre
+        self.id = id
+        self.artist_id = artist_id
