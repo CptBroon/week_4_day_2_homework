42aee5c8d63bc514e0b6c59618de13a199f804ae
Graeme Brown <graemebrown1874@gmail.com>
2021-11-09 20:31:18 +0000

    MVP completed for artists file

--- repositories/artist_repository.py
+++ repositories/artist_repository.py
@@ -2,3 +2,49 @@ from db.run_sql import run_sql
 
 from models.album import Album
 from models.artist import Artist
+
+# CREATE
+def save(artist):
+    sql = f"INSERT INTO artists (name) VALUES (%s) RETURNING *"
+    values = [artist.name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    artist.id = id
+    return artist
+
+# READ
+def select_all():  
+    artists = [] 
+
+    sql = "SELECT * FROM artists"
+    results = run_sql(sql)
+
+    for row in results:
+        artist = Artist(row['name'], row['id'])
+        artists.append(artist)
+    return artists
+
+def select(id):
+    artist = None
+    sql = f"SELECT * FROM artists WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+    if result is not None:
+        artist = Artist(result['name'], result['id'])
+    return artist
+
+# UPDATE
+def update(artist):
+    sql = "UPDATE artists SET (name) = (%s) WHERE id = %s"
+    values = [artist.name, artist.id]
+    run_sql(sql, values)
+    
+# DELETE
+def delete_all():
+    sql = "DELETE FROM artists"
+    run_sql(sql)
+    
+def delete(id):
+    sql = "DELETE FROM artists WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
